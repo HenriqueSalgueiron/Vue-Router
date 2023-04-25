ba4d1e3fbd0200965d32f0468993e6389bba6e33
Henrique Salgueiro <henriquehssjc@gmail.com>
2023-04-25 14:44:34 -0300

    vue project

---
+++ vue-travel-app/src/components/TheNavigation.vue
@@ -0,0 +1,65 @@
+<template>
+  <nav id="nav">
+    <p class="logo">Viage</p>
+    <ul class="nav-links">
+      <li class="links">
+        <router-link to="/">Home</router-link>
+      </li>
+      <li
+        v-for="destination in destinations"
+        :key="destination.name"
+        class="links"
+      >
+        <router-link
+          :to="{
+            name: 'destination.details',
+            params: { slug: destination.slug },
+          }"
+        >
+          {{ destination.name }}
+        </router-link>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+import store from "../store";
+export default {
+  data() {
+    return {
+      destinations: store.destinations,
+    };
+  },
+};
+</script>
+
+<style scoped>
+#nav {
+  display: flex;
+  align-items: center;
+}
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+  text-decoration: none;
+}
+nav a.router-link-exact-active {
+  color: #42b983;
+}
+.nav-links {
+  display: flex;
+}
+.links {
+  padding-right: 20px;
+  list-style: none;
+}
+.links:hover {
+  text-decoration: underline;
+}
+.logo {
+  font-size: 20px;
+  color: purple;
+  font-weight: bold;
+}
+</style>
---
+++ vue-travel-app/src/router/index.js
@@ -0,0 +1,37 @@
+import { createRouter, createWebHashHistory } from "vue-router";
+import HomeView from "../views/HomeView.vue";
+
+const routes = [
+  {
+    path: "/",
+    name: "home",
+    component: HomeView,
+  },
+  {
+    path: "/destinations/:slug",
+    name: "destination.details",
+    props: true,
+    component: () =>
+      import(
+        /* webpackChunkName: "DestinationDetails" */ "../views/DestinationDetails.vue"
+      ),
+    children: [
+      {
+        path: ":experienceSlug",
+        name: "experienceDetails",
+        props: true,
+        component: () =>
+          import(
+            /* webpackChunkName: "DestinationDetails" */ "../views/ExperienceDetails.vue"
+          ),
+      },
+    ],
+  },
+];
+
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes,
+});
+
+export default router;
